ef4165180f85b52d200d6d231e819d7bea2463ce
bdel09 <bmcdelbarrio@gmail.com>
2025-09-16 08:51:30 -0400

    Create student.py

---
+++ student.py
@@ -0,0 +1,7 @@
+#Name: Benjamin Del Barrio
+#Email: Benjamin.delbarrio31@myhunter.cuny.edu
+
+For i in range(5):                            #The most succint way of going about it
+      Phrase = input(“Enter a Phrase: ")      
+      Words = Phrase.split()                  
+      Print(len(Words))
